71ad3bfae3a1cab70de7f6da7990a53d5d8e84ac
Iván Carrascosa Barragán <ivan.carrascosa@iesnervion.es>
2025-11-25 18:08:48 +0100

    Ejercicio 6

--- Ejercicio6/app/(drawer)/_layout.tsx
+++ Ejercicio6/app/(drawer)/_layout.tsx
@@ -2,7 +2,7 @@ import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
 export default function DrawerLayout() {
   return <Drawer
-  initialRouteName="tabs">
+  >
     <Drawer.Screen name="tabs"
                 options={{
                     title: "Home",
